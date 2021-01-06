8586c5de8a2b7398b5e4e9c2fca550c5da2fba3e
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2021-01-06 13:18:54 -0500

    set up methods

--- Node.java
+++ Node.java
@@ -1,9 +1,34 @@
 public class Nano {
-
-    public Node(String value){
-        
-    }
     private String data;
     private Node next,prev;
+
+    public Node(String value){
+        data = value;
+    }
+
+    public String getData() {
+
+    }
+
+    public Node getNext() {
+
+    }
+
+    public Node getPrev() {
+
+    }
+
+    public String setData() {
+
+    }
+
+    public Node setNext() {
+
+    }
+
+    public Node setPrev() {
+
+    }
+
     //write get/set methods for all three instance variables.
 }
