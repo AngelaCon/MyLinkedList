58342498e8f1231298fc96a5564ab5093edca37e
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2021-01-06 13:05:20 -0500

    set up node

---
+++ Node.java
@@ -0,0 +1,7 @@
+public class Nano {
+
+    public Node(String value){/*create a constructor*/}
+    private String data;
+    private Node next,prev;
+    //write get/set methods for all three instance variables.
+}
