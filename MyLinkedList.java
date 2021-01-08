5eb06cbf7daae69b9fd84f86ac71bb67e6f35317
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2021-01-08 01:34:01 -0500

    partially done with methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -3,11 +3,54 @@ public class MyLinkedList {
     private int size;
     private Node start,end;
     public MyLinkedList(){
+        size = 0;
+
     }
-    public int size();
-    public boolean add(String value);
-    public boolean add(int index, String value);
-    public String get(int index);
-    public String set(int index, String value);
-    public String toString();
+    public int size() {
+        return size;
+    }
+
+    public boolean add(String value) {
+        Node newNode = new Node(value);
+        if (size == 0) {
+            start = newNode;
+            end = newNode;
+            size++;
+        }
+        else {
+            end.setNext(newNode);
+           newNode.setPrev(end);
+           end = newNode;
+           size++;
+        }
+        return true;
+    }
+    public boolean add(int index, String value) {
+
+
+    }
+    public String get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        return getNodeAtIndex(index).getData();
+
+    }
+    public String set(int index, String value) {
+        Node current = start;
+        if (index < 0 || index >= size)throw new IndexOutOfBoundsException();
+        for(int i = 0;i<index;i++){
+            current = current.getNext();
+        }
+        return current.getData();
+    }
+
+    public String toString() {
+        String s = "[";
+        for(int i  = 0; i< this.size; i++){
+            s = s + this.get(i);
+            if(i!= this.size-1) s+= ", ";
+            else s+= "]";
+        }
+        if (size == 0) s = "[]";
+        return s;
+    }
 }
